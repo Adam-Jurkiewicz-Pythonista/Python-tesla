d98513e40067238b2bbc6879363630bc0023975e
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-21 09:34:21 +0200

    dzien_02

--- dzien_02/web_02.py
+++ dzien_02/web_02.py
@@ -11,9 +11,15 @@ def hello_user(name):
     plec = "Dziewczynę" if name.endswith("a") else "Chłopaka"
     return f"<h2>Hej {name} - wyglądasz na {plec}.</h2>"
 
-@app.route("/user/")
+# http://localhost:5000/user2?imie=Adasiek&rok=1974
+@app.route("/user2")
 def hello_user_request():
     name = request.args.get("imie", "NIC")
-    return f"<h1>Halo -- {name}!</h1>"
+    rok = request.args.get("rok", 0)
+    if rok.isdigit():
+        wiek = 2025 - int(rok)
+        return f"<h1>Halo -- {name} / {rok=} / {wiek=} </h1>"
+    else:
+        return "<h3>Zła wartość roku</h3>"
 
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000, debug=True)
