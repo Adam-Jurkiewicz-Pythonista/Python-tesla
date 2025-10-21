b9c18f96ca84aa777e28601f588687b24296d339
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-21 11:10:04 +0200

    templaty

---
+++ dzien_02/docker_app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from markupsafe import escape
+
+app = Flask("APP_Template")
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/user/<name>")
+def hello_user(name):
+    plec = "Dziewczynę" if name.endswith("a") else "Chłopaka"
+    return render_template("zmienna.html", name=name, plec=plec)
+
+
+# dla dockera nie jest to ważne
+# app.run(host="0.0.0.0", port=5000, debug=True)
