f9da5c4ff92a4899b7ac86eebdbf7b93a154af46
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-21 12:16:17 +0200

    templaty z docker'a

---
+++ dzien_02/docker_app_formularz/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+from markupsafe import escape
+
+app = Flask("APP_Template")
+
+@app.route("/")
+def hello_world():
+    return render_template("formularz_rejestracji.html")
+
+@app.route("/rejestruj")
+def rejestruj():
+    return "NIC"
+
+
+@app.route("/user/<name>")
+def hello_user(name):
+    plec = "Dziewczynę" if name.endswith("a") else "Chłopaka"
+    return render_template("zmienna.html", name=name, plec=plec)
+
+
+# dla dockera nie jest to ważne
+# app.run(host="0.0.0.0", port=5000, debug=True)
