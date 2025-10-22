9496bc1cbe38afb2596f831ee038806d95b4f822
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-22 09:28:50 +0200

    fn import

--- dzien_03/importy/import_01.py
+++ dzien_03/importy/import_01.py
@@ -1,1 +1,11 @@
 # tu chcemy wykorzystać funkcje
+
+# plik o nazwie funkcje.py musi być w tym samym katalogu
+from funkcje import *
+
+while True:
+    passwd = input("Podaj hasło (END) :")
+    if passwd == "END":
+        break
+    _, hash = hash_password(passwd)
+    print(f"Zaszyfrowane hasło: {hash=}")
