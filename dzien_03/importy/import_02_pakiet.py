5026842833661b08bb117c0509a665d2c9c67efd
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-22 09:33:32 +0200

    fn import

---
+++ dzien_03/importy/import_02_pakiet.py
@@ -0,0 +1,11 @@
+# tu chcemy wykorzystać funkcje
+
+# plik z funkcjami o nazwie szyfrowanie w pakiecie dodatki
+from dodatki.szyfrowanie import *
+
+while True:
+    passwd = input("Podaj hasło (END) :")
+    if passwd == "END":
+        break
+    _, hash = hash_password(passwd)
+    print(f"Zaszyfrowane hasło: {hash=}")
