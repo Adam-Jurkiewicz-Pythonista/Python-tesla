e770f53dc7026c67f78be9d66fb058100d83f025
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-22 11:38:59 +0200

    fn import

---
+++ dzien_03/exe_windows/test_pyinstallera.py
@@ -0,0 +1,9 @@
+from nicegui import ui
+
+# for Linux to work with native=True
+import multiprocessing
+multiprocessing.set_start_method("spawn", force=True)
+###
+
+ui.label('test pyinstallera!')
+ui.run(native=True)
