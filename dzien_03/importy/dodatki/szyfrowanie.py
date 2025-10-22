fb31b3b9883936835b739b08446d4bb8fa577907
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-22 10:06:11 +0200

    fn import

--- dzien_03/importy/dodatki/szyfrowanie.py
+++ dzien_03/importy/dodatki/szyfrowanie.py
@@ -13,11 +13,10 @@ def hash_password(password, salt_bits=32, nr_iterations=100000):
     # Generate a random salt
     salt = os.urandom(salt_bits)
     hashed = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, iterations=nr_iterations)
-    return salt, hashed
     # Convert the hash and salt to hexadecimal strings for storage
     return binascii.hexlify(salt).decode(), binascii.hexlify(hashed).decode()
 
-def verify_password(stored_salt: str, stored_hash: str, input_password: str) -> bool:
+def verify_password(stored_salt: str, stored_hash: str, input_password: str, nr_iter = 1000) -> bool:
     salt = binascii.unhexlify(stored_salt)
     hashed = hashlib.pbkdf2_hmac('sha256', input_password.encode(), salt, 100000)
     return binascii.hexlify(hashed).decode() == stored_hash
