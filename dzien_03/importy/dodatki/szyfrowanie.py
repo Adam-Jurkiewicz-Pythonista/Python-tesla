20bb34e5961057612e442830c02cb3e4513a7965
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-10-22 09:45:17 +0200

    fn import

--- dzien_03/importy/dodatki/szyfrowanie.py
+++ dzien_03/importy/dodatki/szyfrowanie.py
@@ -2,11 +2,18 @@ import os
 import hashlib
 import binascii
 
-def hash_password(password):
+def hash_password(password, salt_bits=32, iterations=100000):
+    """
+    PBKDF2-HMAC-SHA256 hash function
+    :param password:
+    :param salt_bits: bits for urandom, default 32
+    :param iterations: number of iterations, default 100000
+    :return: tuple (salt, hashed pass with hex)
+    """
     # Generate a random salt
-    salt = os.urandom(16)
-    # Use PBKDF2-HMAC-SHA256 with 100,000 iterations
-    hashed = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
+    salt = os.urandom(salt_bits)
+    hashed = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, iterations=iterations)
+    return salt, hashed
     # Convert the hash and salt to hexadecimal strings for storage
     return binascii.hexlify(salt).decode(), binascii.hexlify(hashed).decode()
 
